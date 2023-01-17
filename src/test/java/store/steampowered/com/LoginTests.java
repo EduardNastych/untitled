0d4dd27f6d6888b017cff093225a3cf6e180d6e6
Eduard <ed27nast@gmail.com>
2023-01-17 21:59:44 +0200

    Added LoginTests, deleted NegativeTests and PositiveTests.

--- src/test/java/store/steampowered/com/LoginTests.java
+++ src/test/java/store/steampowered/com/LoginTests.java
@@ -8,41 +8,9 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class NegativeTests extends TestInit{
-   /* @Test(priority = 1, groups = { "negativeTests", "smokeTests" })
-    public void incorrectLogin(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        String url = "https://store.steampowered.com/";
-        driver.get(url);
-
-        String expectedURL = "https://store.steampowered.com/";
-        String actualURL = driver.getCurrentUrl();
-        Assert.assertEquals(actualURL, expectedURL, "Actual page URL is not the same as expected.");
-
-        sleep(5);
-        driver.manage().window().maximize();
-        sleep(5);
-        driver.findElement(By.xpath("//a[text()='увійти']")).click();
-        sleep(5);
-
-        WebElement logIn = driver.findElement(By.xpath("//button[text()='Увійти']"));
-        Assert.assertTrue(logIn.isDisplayed(), "LogIn button is not visible.");
-
-        WebElement username = driver.findElement(By.xpath("(//input[@class='newlogindialog_TextInput_2eKVn'])[1]"));
-        username.sendKeys("eduardnastych");
-        sleep(5);
-        WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
-        password.sendKeys("eduard27072002");
-        sleep(5);
-        logIn.click();
-        sleep(5);
-
-        WebElement popUpNotification = driver.findElement(By.xpath("//div[text()='Будь ласка, перевірте логін і пароль свого акаунта, а потім спробуйте знову.']"));
-        Assert.assertTrue(popUpNotification.isDisplayed(), "Pop-up notification isn`t visible");
-    }
-    @Test(priority = 2, groups = { "negativeTests" })
-    public void incorrectPassword(){
+public class LoginTests extends TestInit {
+    @Test(priority = 1, groups = { "positiveTests", "smokeTests" })
+    public void positiveLoginTests(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String url = "https://store.steampowered.com/";
@@ -65,22 +33,22 @@ public class NegativeTests extends TestInit{
         username.sendKeys("anyashyta");
         sleep(5);
         WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
-        password.sendKeys("petro1234");
+        password.sendKeys("eduard27072002");
         sleep(5);
         logIn.click();
         sleep(5);
+        driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys("6");
+        driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys("C");
+        driver.findElement(By.xpath("(//input[@type='text'])[3]")).sendKeys("4");
+        driver.findElement(By.xpath("(//input[@type='text'])[4]")).sendKeys("C");
+        driver.findElement(By.xpath("(//input[@type='text'])[5]")).sendKeys("4");
+        driver.quit();
 
-        WebElement popUpNotification = driver.findElement(By.xpath("//div[text()='Будь ласка, перевірте логін і пароль свого акаунта, а потім спробуйте знову.']"));
-        String expectedpopUpNotification = "Будь ласка, перевірте логін і пароль свого акаунта, а потім спробуйте знову.";
-        String actualpopUpNotification = popUpNotification.getText();
-        Assert.assertEquals(actualpopUpNotification, expectedpopUpNotification, "Expected massage isn`t displayed" );
     }
-*/
-
 
     @Parameters({ "username", "password", "expectedpopUpNotification" })
-    @Test
-    public void incorrectLoginAndPassword(String username, String password, String expectedpopUpNotification){
+    @Test(priority = 2, groups = { "negativeTests", "smokeTests" })
+    public void negativeLoginTest(String username, String password, String expectedpopUpNotification){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String url = "https://store.steampowered.com/";
@@ -113,5 +81,8 @@ public class NegativeTests extends TestInit{
 
         String actualpopUpNotification = popUpNotification.getText();
         Assert.assertEquals(actualpopUpNotification, expectedpopUpNotification, "Expected message isn`t displayed" );
+
+        driver.quit();
     }
 }
+
